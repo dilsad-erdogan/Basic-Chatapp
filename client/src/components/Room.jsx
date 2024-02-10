01b80628bc033e4d269f096a577110a1d184dcf0
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-02-11 01:10:31 +0300

    11.02 commit

---
+++ client/src/components/Room.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Room = ({ username, room, setUsername, setRoom, setChatScreen, socket}) => {
+
+    const sendRoom = () => {
+        socket.emit('room', room)
+        setChatScreen(true)
+    }
+
+    return (
+        <div className='flex items-center justify-center h-full'>
+            <div className="w-1/3 h-[320px] rounded-l bg-indigo-600 flex flex-col space-y-4 p-3">
+                <h1 className='text-center my-4 font-bold text-2xl'>Welcome To Chat App</h1>
+                <input value={username} onChange={e => setUsername(e.target.value)} className='h-12 rounded-xl p-3 outline-none' type='text' placeholder='Username'></input>
+                <input value={room} onChange={e => setRoom(e.target.value)} className='h-12 rounded-xl p-3 outline-none' type="text" placeholder='Room'/>
+                <div onClick={sendRoom} className='tracking-wider hover:opacity-70 cursor-pointer text-white bg-indigo-900 h-12 pt-2 test-xl text-center rounded-xl'>Chat!!!</div>
+            </div>
+        </div>
+    )
+}
+
+export default Room
